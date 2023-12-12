e0fa0a4493bb5025216153d259d70a5a70719b3b
Jannis Cloodt <j.cloodt@micromata.de>
2023-12-12 11:51:23 +0100

    updated database shema

--- backend/db/migration/V1_0__create_user_schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `user` (
-
-    `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` varchar(20),
-    `email` varchar(50),
-    `date_of_birth` timestamp
-
-    )ENGINE=InnoDB DEFAULT CHARSET=UTF8;
